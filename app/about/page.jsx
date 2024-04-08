72c7aab7dc9d502b7e3925fcce43a3ae75e7f1b0
Gustavo Kumagai <iagamuk.gus@gmail.com>
2024-04-08 14:03:56 -0300

    V1.0

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <div className="bg-transparent mb-8 sm:bg-dark p-2 rounded-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
           <h3 className="text-sm text-secondary-contrast">Formação</h3>
-          <h2 className="text-lg font-semibold">
+          <h2 className="text-lg font-semibold text-primary-contrast">
             Análise e Desenvolvimento de Sistemas
           </h2>
           <p className="text-sm text-secondary-contrast">
@@ -19,7 +19,9 @@ const About = () => {
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
           <h3 className="text-sm text-secondary-contrast">Experiência</h3>
-          <h2 className="text-lg font-semibold">Estagiário Administrativo</h2>
+          <h2 className="text-lg font-semibold text-primary-contrast">
+            Estagiário Administrativo
+          </h2>
           <p className="text-sm text-secondary-contrast">
             Corpo de Bombeiros de Umuarama
           </p>
@@ -37,55 +39,55 @@ const About = () => {
       </h1>
       <div className="bg-transparent sm:bg-dark p-2 rounded-lg grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">HTML & CSS</h2>
+          <h2 className="text-lg text-primary-contrast">HTML & CSS</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Intermediário
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">PHP</h2>
+          <h2 className="text-lg text-primary-contrast">PHP</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Intermediário
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">SQL</h2>
+          <h2 className="text-lg text-primary-contrast">SQL</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Intermediário
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">NextJS</h2>
+          <h2 className="text-lg text-primary-contrast">NextJS</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Báscio
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">ReactJS</h2>
+          <h2 className="text-lg text-primary-contrast">ReactJS</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Báscio
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">Tailwind</h2>
+          <h2 className="text-lg text-primary-contrast">Tailwind</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Báscio
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">NodeJS</h2>
+          <h2 className="text-lg text-primary-contrast">NodeJS</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Báscio
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">Git</h2>
+          <h2 className="text-lg text-primary-contrast">Git</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Básico
           </h3>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h2 className="text-lg">Inglês</h2>
+          <h2 className="text-lg text-primary-contrast">Inglês</h2>
           <h3 className="text-sm text-secondary-contrast font-semibold mb-2">
             Avançado
           </h3>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       </h1>
       <div className="bg-transparent sm:bg-dark p-2 rounded-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h3 className="text-lg mb-2">Whatsapp</h3>
+          <h3 className="text-lg mb-2 text-primary-contrast">Whatsapp</h3>
           <a
             href="https://wa.me/5544988150190"
             className="text-green-400 text-md bg-transparent hover:bg-primary border-green-400 border-2 rounded-md p-1"
@@ -17,7 +17,7 @@ const Contact = () => {
           </a>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h3 className="text-lg mb-2">GitHub</h3>
+          <h3 className="text-lg mb-2 text-primary-contrast">GitHub</h3>
           <a
             href="https://github.com/TofuVoador"
             className="text-gray-400 text-md bg-transparent hover:bg-primary border-gray-400 border-2 rounded-md p-1"
@@ -26,7 +26,7 @@ const Contact = () => {
           </a>
         </div>
         <div className="bg-secondary p-4 rounded-lg shadow-sm shadow-black">
-          <h3 className="text-xl mb-2">Email</h3>
+          <h3 className="text-xl mb-2 text-primary-contrast">Email</h3>
           <a
             href="mailto:iagamuk.gus@gmail.com"
             className="text-red-400 bg-transparent hover:bg-primary border-red-400 border-2 rounded-md p-1"
